c677ee276961f114354bd00b71fd058d50913eb9
viniciussslima <viniciussslima999@gmail.com>
2022-10-15 14:01:01 -0300

    feat: :sparkles: added function to transform phone string into object

---
+++ src/phone/toObject.test.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect } from 'vitest';
+
+import { toObject } from './toObject';
+
+describe('transform object into phone number', () => {
+  it('should transform phone with mask into object', () => {
+    expect(toObject('+55 (84) 98877-6655')).toEqual({
+      areaCode: '55',
+      countryCode: '84',
+      number: '988776655',
+    });
+  });
+
+  it('should transform phone without mask into object', () => {
+    expect(toObject('5584988776655')).toEqual({
+      areaCode: '55',
+      countryCode: '84',
+      number: '988776655',
+    });
+  });
+
+  it('should transform phone with mask and without area code into object', () => {
+    expect(toObject('(84) 98877-6655')).toEqual({
+      countryCode: '84',
+      number: '988776655',
+    });
+  });
+
+  it('should transform phone without mask and area code into object', () => {
+    expect(toObject('84988776655')).toEqual({
+      countryCode: '84',
+      number: '988776655',
+    });
+  });
+
+  it('should transform phone with mask and only the number into object', () => {
+    expect(toObject('98877-6655')).toEqual({
+      number: '988776655',
+    });
+  });
+
+  it('should transform phone without mask and only the number into object', () => {
+    expect(toObject('988776655')).toEqual({
+      number: '988776655',
+    });
+  });
+
+  it('should throw error if phone is invalid', () => {
+    expect(() => toObject('98877665')).toThrow('Invalid phone');
+  });
+});
