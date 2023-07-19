1525998db7e6f719c667bc2081fb9d5c9414a7cc
Vinicius Santos <viniciussslima999@gmail.com>
2023-07-19 14:31:43 -0300

    test: :white_check_mark: added testes for phone validation

--- src/phone/validate.test.ts
+++ src/phone/validate.test.ts
@@ -7,28 +7,52 @@ describe('validate phone', () => {
     expect(validate('5584988776655')).toBeTruthy();
   });
 
+  it('should 558488776655 be valid', () => {
+    expect(validate('558488776655')).toBeTruthy();
+  });
+
   it('should (84) 98877-6655 be valid', () => {
     expect(validate('+55 (84) 98877-6655')).toBeTruthy();
+  });
+
+  it('should (84) 8877-6655 be valid', () => {
+    expect(validate('+55 (84) 8877-6655')).toBeTruthy();
   });
 
   it('should 84988776655 be valid', () => {
     expect(validate('84988776655')).toBeTruthy();
   });
 
+  it('should 8488776655 be valid', () => {
+    expect(validate('8488776655')).toBeTruthy();
+  });
+
   it('should (84) 98877-6655 be valid', () => {
     expect(validate('(84) 98877-6655')).toBeTruthy();
+  });
+
+  it('should (84) 8877-6655 be valid', () => {
+    expect(validate('(84) 8877-6655')).toBeTruthy();
   });
 
   it('should 988776655 be valid', () => {
     expect(validate('988776655')).toBeTruthy();
   });
 
-  it('should 8498877665 not be valid', () => {
-    expect(validate('8498877665')).toBeFalsy();
+  it('should 88776655 be valid', () => {
+    expect(validate('88776655')).toBeTruthy();
+  });
+
+  it('should 8877665 not be valid', () => {
+    expect(validate('8877665')).toBeFalsy();
   });
 
   it('should 98877-6655 not be valid', () => {
-    expect(validate('988-776655')).toBeTruthy();
+    expect(validate('98877-6655')).toBeTruthy();
+  });
+
+  it('should 8877-6655 not be valid', () => {
+    expect(validate('8877-6655')).toBeTruthy();
   });
 
   it('should empty phone not be valid', () => {
