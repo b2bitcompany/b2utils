7e21c193c9b9466fc45690b91ed9b83d807c2b3e
Hudson Bruno <hb.natal@gmail.com>
2024-02-08 10:03:25 -0300

    fix: :sparkles: remove dot from cep mask

--- src/cep/mask.ts
+++ src/cep/mask.ts
@@ -1,7 +1,6 @@
 export const mask = (value: string) => {
   return value
     .replace(/\D/g, '')
-    .replace(/^(\d{2})(\d)/, '$1.$2')
-    .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2-$3')
+    .replace(/^(\d{5})(\d)/, '$1-$2')
     .replace(/(-\d{3})\d+?$/, '$1');
 };
