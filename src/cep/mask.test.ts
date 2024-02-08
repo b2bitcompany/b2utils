b0fa30d38df280f35d9a55080136be1b1c70b15e
Hudson Bruno <hb.natal@gmail.com>
2024-02-08 10:24:05 -0300

    test: :white_check_mark: removed dot from cep tests

--- src/cep/mask.test.ts
+++ src/cep/mask.test.ts
@@ -3,11 +3,11 @@ import { describe, it, expect } from 'vitest';
 import { mask } from './mask';
 
 describe('mask CEP', () => {
-  it('should 12345678 be transform 12.345-678', () => {
-    expect(mask('12345678')).toBe('12.345-678');
+  it('should 12345678 be transform 12345-678', () => {
+    expect(mask('12345678')).toBe('12345-678');
   });
 
-  it('should 123456 be transform 12.345-6', () => {
-    expect(mask('123456')).toBe('12.345-6');
+  it('should 123456 be transform 12345-6', () => {
+    expect(mask('123456')).toBe('12345-6');
   });
 });
